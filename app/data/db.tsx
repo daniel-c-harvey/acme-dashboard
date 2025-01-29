3a7c25fd797add6b16734eec7f9e0f564cf6415e
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-01-29 05:57:58 -0500

    Chapter 6: Postgresql Database integration

---
+++ app/data/db.tsx
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import postgres from 'postgres';
+
+// Ensure required environment variables are present
+if (!process.env.POSTGRES_USER || 
+    !process.env.POSTGRES_HOST || 
+    !process.env.POSTGRES_PASSWORD || 
+    !process.env.POSTGRES_DATABASE) {
+    throw new Error('Missing required database environment variables');
+}
+
+const sql = postgres({
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE
+});
+
+export default sql;
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,12 +1,11 @@
 // import bcrypt from 'bcrypt';
-// import { db } from '@vercel/postgres';
+// import sql from '@/app/data/db';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
-
-// const client = await db.connect();
-
-// async function seedUsers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await client.sql`
+// import { Sql } from 'postgres';
+//
+// async function seedUsers(sql: Sql) {
+//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//   await sql`
 //     CREATE TABLE IF NOT EXISTS users (
 //       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 //       name VARCHAR(255) NOT NULL,
@@ -14,25 +13,25 @@
 //       password TEXT NOT NULL
 //     );
 //   `;
-
+//
 //   const insertedUsers = await Promise.all(
 //     users.map(async (user) => {
 //       const hashedPassword = await bcrypt.hash(user.password, 10);
-//       return client.sql`
+//       return sql`
 //         INSERT INTO users (id, name, email, password)
 //         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
 //         ON CONFLICT (id) DO NOTHING;
 //       `;
 //     }),
 //   );
-
+//
 //   return insertedUsers;
 // }
-
-// async function seedInvoices() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-//   await client.sql`
+//
+// async function seedInvoices(sql: Sql) {
+//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//
+//   await sql`
 //     CREATE TABLE IF NOT EXISTS invoices (
 //       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 //       customer_id UUID NOT NULL,
@@ -41,24 +40,24 @@
 //       date DATE NOT NULL
 //     );
 //   `;
-
+//
 //   const insertedInvoices = await Promise.all(
 //     invoices.map(
-//       (invoice) => client.sql`
+//       (invoice) => sql`
 //         INSERT INTO invoices (customer_id, amount, status, date)
 //         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
 //         ON CONFLICT (id) DO NOTHING;
 //       `,
 //     ),
 //   );
-
+//
 //   return insertedInvoices;
 // }
-
-// async function seedCustomers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-//   await client.sql`
+//
+// async function seedCustomers(sql: Sql) {
+//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//
+//   await sql`
 //     CREATE TABLE IF NOT EXISTS customers (
 //       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 //       name VARCHAR(255) NOT NULL,
@@ -66,57 +65,51 @@
 //       image_url VARCHAR(255) NOT NULL
 //     );
 //   `;
-
+//
 //   const insertedCustomers = await Promise.all(
 //     customers.map(
-//       (customer) => client.sql`
+//       (customer) => sql`
 //         INSERT INTO customers (id, name, email, image_url)
 //         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
 //         ON CONFLICT (id) DO NOTHING;
 //       `,
 //     ),
 //   );
-
+//
 //   return insertedCustomers;
 // }
-
-// async function seedRevenue() {
-//   await client.sql`
+//
+// async function seedRevenue(sql: Sql) {
+//   await sql`
 //     CREATE TABLE IF NOT EXISTS revenue (
 //       month VARCHAR(4) NOT NULL UNIQUE,
 //       revenue INT NOT NULL
 //     );
 //   `;
-
+//
 //   const insertedRevenue = await Promise.all(
 //     revenue.map(
-//       (rev) => client.sql`
+//       (rev) => sql`
 //         INSERT INTO revenue (month, revenue)
 //         VALUES (${rev.month}, ${rev.revenue})
 //         ON CONFLICT (month) DO NOTHING;
 //       `,
 //     ),
 //   );
-
+//
 //   return insertedRevenue;
 // }
-
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
-
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
-}
+//
+// export async function GET() {
+//     try {
+//       await sql.begin(async (sql : Sql) => {
+//         await seedUsers(sql);
+//         await seedCustomers(sql);
+//         await seedInvoices(sql);
+//         await seedRevenue(sql);
+//       });
+//       return Response.json({ message: 'Database seeded successfully' });
+//     } catch (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
+// }
