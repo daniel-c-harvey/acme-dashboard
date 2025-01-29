931d7e9740898eafecc1c50573891da7e61c94a7
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-01-29 01:52:36 -0500

    Chapter 3: Fonts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Arvo, Inter, Lusitana, Courier_Prime } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight: '400', subsets: ['latin']});
+export const courier_prime = Courier_Prime({weight: '400', subsets: ['latin']});
+export const arvo = Arvo({weight: '400', subsets: ['latin']});
