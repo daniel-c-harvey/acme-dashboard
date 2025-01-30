aaf94ffb8ab80f1d90db6449dbfb990ee94b363d
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-01-30 15:07:37 -0500

    Deployment Configs

--- dch5-deploy.sh
+++ dch5-deploy.sh
@@ -1,21 +1,30 @@
-# Compile and Package
-next build
+# Start ssh-agent and add key
+eval $(ssh-agent -s)
+ssh-add /c/.ssh/acme_ed25519
 
 # Prepare Output Directories
 mkdir deploy
 cd ./deploy
-mkdir ./.next && mkdir ./public
+mkdir ./app && mkdir ./public
 
-cp -r ../.next/* ./.next
-cp -r ../public ./public
-cp ../package.json ./ && cp ../package-lock.json ./ && cp ../next.config.ts ./
+# Copy
+cp -r ../app/* ./app
+cp -r ../public/* ./public
+cp ../tsconfig.json ./ && cp ../package.json ./ && cp ../package-lock.json ./
+cp ../next.config.ts ./ && cp ../next-env.d.ts ./
+cp ../postcss.config.js ./ && cp ../tailwind.config.ts ./
 
-KEY="-i /c/.ssh/acme_ed25519"
+# Deploy
 REMOTE="acme@dch5.snailbird.net"
-REMOTEKEY="$KEY $REMOTE"
-APPROOT="/acme-dashboard/bin"
+APPROOT="/acme-dashboard"
 
-ssh $REMOTEKEY "rm -rf $APPROOT/*"
-scp $KEY -r ./ $REMOTE:$APPROOT
+ssh $REMOTE "rm -rf $APPROOT/src/*"
+scp -r ./ $REMOTE:$APPROOT/src
+ssh $REMOTE "cp $APPROOT/.env $APPROOT/src/"
 
+# Build the deployed source on the deployment environment
+ssh $REMOTE "cd $APPROOT && ./build.sh && ./restart.sh"
+
+# Clean up
 rm -rf ../deploy
+ssh-agent -k
